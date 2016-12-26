2a84d84d12d60d86091fb1850abca23f5b6aa292
Rune Strand <rune@umbraco.com>
2016-12-27 00:27:53 +0100

    Grid CTA Button 0.1

---
+++ GridCTA/GridCTA.controller.js
@@ -0,0 +1,34 @@
+﻿
+(function () {
+    "use strict";
+
+    function Controller($scope) {
+
+        $scope.openOverlay = openOverlay;
+
+        if (!$scope.control.value || $scope.control.value == "") {
+            $scope.control.value = $scope.control.editor.config.btnDefaults;
+        }
+
+        function openOverlay() {
+
+            $scope.overlay = {
+                title: "Edit CTA Button",
+                view: "/App_Plugins/GridCTA/GridCTAOverlay.html",
+                editModel: $scope.control.value,
+                btnColor: $scope.control.editor.config.btnColor,
+                show: true,
+                submit: function (model) {
+                    $scope.overlay.show = false;
+                    $scope.overlay = null;
+                },
+                close: function (oldModel) {
+                    $scope.overlay.show = false;
+                    $scope.overlay = null;
+                }
+            }
+        }
+    }
+
+    angular.module("umbraco").controller("rhs.gridctacontroller", Controller);
+})();
